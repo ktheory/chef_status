efda9d329f9ce48c22b5426e11523ba6e6281f22
Aaron Suggs <aaron@ktheory.com>
2011-03-31 18:17:04 -0400

    Debugging postgresql time issues.

--- app/models/chef_report.rb
+++ app/models/chef_report.rb
@@ -1,8 +1,8 @@
 class ChefReport < ActiveRecord::Base
 
   default_scope order('node ASC')
-  scope :live,      lambda { where("updated_at >= ?", Time.now - REPORTING_INTERVAL) }
-  scope :offline,   lambda { where("updated_at < ?", Time.now - REPORTING_INTERVAL) }
+  scope :live,      lambda { where("updated_at >= ?", Time.now.utc - REPORTING_INTERVAL) }
+  scope :offline,   lambda { where("updated_at <  ?", Time.now.utc - REPORTING_INTERVAL) }
   scope :converged, live.where(:success => true)
   scope :failing,   live.where(:success => false)
 
